4da13e656c0454653e66861824253849dc451a46
Felipe Renan <felipeferreira3146@gmail.com>
2025-04-10 17:33:41 -0300

    feat: configure Swagger and SwaggerUI for endpoint documentation

--- TeamManager/Program.cs
+++ TeamManager/Program.cs
@@ -1,23 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using TeamManager.Data;
 using TeamManager.Model;
+using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(opt => 
     opt.UseInMemoryDatabase("TeamManager"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
