f79d95c597278e4e434518e058999e88fa67e186
Felipe Renan <felipeferreira3146@gmail.com>
2025-05-26 23:52:09 -0300

    feat: create Profile for Coache

---
+++ TeamManager.Application/Mapping/CoachProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TeamManager.Domain.DTOs;
+using TeamManager.Domain.Model;
+
+namespace TeamManager.Application.Mapping;
+
+public class CoachProfile : Profile
+{
+    public CoachProfile()
+    {
+        CreateMap<CoachCreateDto, Coach>();
+
+        CreateMap<CoachUpdateDto, Coach>();
+
+        CreateMap<Coach, CoachResponseDto>()
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.ToString()));
+    }
+}
